9104cef33f1dd171008d01d93c97e7afd73f315a
Girish <girish.fuluskar@gmail.com>
2018-09-04 21:42:36 +0530

    Changes

--- src/android/NetworkMeter.java
+++ src/android/NetworkMeter.java
@@ -187,12 +187,18 @@ public class NetworkMeter extends CordovaPlugin {
             mDownloadSpeedWithDecimals = (float) mDownloadSpeed / (float) 1000000;
             mUnits = " MB";
 
-        } else {
+        } else if (mDownloadSpeed >= 1000) {
             mDownloadSpeedWithDecimals = (float) mDownloadSpeed / (float) 1000;
             mUnits = " KB";
-        }
+        } else {
+			mDownloadSpeedWithDecimals = (float) mDownloadSpeed;
+            mUnits = " BY";
+		}
+			
 
         if (!mUnits.equals(" KB") && mDownloadSpeedWithDecimals < 100) {
+            mDownloadSpeedOutput = String.format(Locale.US, "%.1f", mDownloadSpeedWithDecimals) + " " + mUnits;
+        } else if (!mUnits.equals(" BY")) {
             mDownloadSpeedOutput = String.format(Locale.US, "%.1f", mDownloadSpeedWithDecimals) + " " + mUnits;
         } else {
             mDownloadSpeedOutput = Integer.toString((int) mDownloadSpeedWithDecimals)+ " " + mUnits;
